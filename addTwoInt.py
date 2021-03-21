a5f83744c2b1ab6071c2bf452ee1bb2579e18cb9
via <via24camadoo@gmail.com>
2021-03-21 10:57:54 +0400

    [ADDITION] Adding 2 positive numbers ONLY

---
+++ addTwoInt.py
@@ -0,0 +1,15 @@
+#!/bin/bash/env python3
+def addint(number1,number2):
+    result = number1 + number2
+    return result
+
+num1 = float(input("Enter the first number : "))
+while(num1 < 0):
+    print("-----Please enter positive numbers ONLY-----")
+    num1 = float(input("Enter the first number : "))
+print()
+num2 = float(input("Enter the Second number : "))
+while(num2 < 0):
+    print("-----Please enter positive numbers ONLY-----")
+    num2 = float(input("Enter the second number : "))
+print(addint(num1,num2))
