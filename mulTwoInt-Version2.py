138b86de3946a1b14875d476b1e1b326e9b57eea
AbhayGunputh <kumarabhay150499@gmail.com>
2021-03-21 20:24:57 +0400

    [multiplication of 2 numbers] With validation

---
+++ mulTwoInt-Version2.py
@@ -0,0 +1,22 @@
+#!/bin/bash/env python3
+number1 = []
+number2 = []
+def mul(a,b):
+	result = a*b
+	return result
+def main():
+	while True:
+		try:
+			first = int(input("Enter the first number : "))
+			sec = int(input("Enter the second number : "))
+			num1 = number1.append(first)
+			num2 = number2.append(sec)
+			print(mul(number1[0],number2[0]))
+			break
+		except:
+			print("You have enter more than one number or 0 number")
+
+print("-----MULTIPLICATION OF 2 NUMBERS-----")
+main()
+
+
