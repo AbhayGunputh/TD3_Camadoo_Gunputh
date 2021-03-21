7cf2e45a8bef6af41dbc6d9da97362d24066b7cc
via <via24camadoo@gmail.com>
2021-03-21 20:31:21 +0400

    [ADDITION] Adding 2 positive numbers ONLY

--- addTwoInt.py
+++ addTwoInt.py
@@ -4,14 +4,14 @@ def addint(number1,number2):
     result = number1 + number2;
     return result;
 
-def main():
-	import sys
-	print (sys.argv)
-	print ("sys.argv)
+num1 = float(input("Enter the first number : "))
+while (num1 < 0):
+	print("-----please enter positive number ONLY-----")
+	num1 = float(input("Enter the first number : "))
+print()
+num2 = float(input("Enter the second number : "))
+while(num2 < 0):
+	print("-----(Please enter positive numbers ONLY-----")
+	num2 = float(input("Enter the second number : "))
+print (addint(num1,num2))
 
-	x = int (sys.argv[1])
-	y = int (sys.argv[2])
-		print(addint(number1,number2))
-
-		print(addint(number1,number2))
-main()
